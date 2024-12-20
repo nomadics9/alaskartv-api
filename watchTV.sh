054db2b11c9822969f80c08fdaa6ed2aef101ea9
nomadics9 <binaskar9@gmail.com>
2024-12-20 19:47:44 +0300

    fix: script sh compliance

--- watchTV.sh
+++ watchTV.sh
@@ -7,8 +7,12 @@ fi
 LAST_RELEASE_FILE=/data/alaskartv/androidtv-ci/release.txt
 
 latest_release=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name')
-
-if [ "$latest_release" != "$(cat $LAST_RELEASE_FILE 2>/dev/null)" ]; then
+if [ -f "$LAST_RELEASE_FILE" ]; then
+    read -r version < "$LAST_RELEASE_FILE"
+else
+    version=""
+fi
+if [ "$latest_release" != "$version" ]; then
   # echo "$latest_release" > "$LAST_RELEASE_FILE"
   curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
     -d chat_id="$CHAT_ID" \
